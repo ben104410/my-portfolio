a509ea07c21e4f4541601ace91686b12431b8700
benjamin <titusben64@gmail.com>
2025-05-26 15:16:21 +0300

    retrigger deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "<my-portfolio", // Replace <repo-name> with your GitHub repo name
+  base: '/', // ✅ Important for Vercel or localhost
   plugins: [react()],
 });
+
